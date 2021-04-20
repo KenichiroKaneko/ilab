7f28929cb0c91e990a2104395c5389e89495fc44
KenichiroKaneko <fanta.remio.ken@gmail.com>
2021-04-20 18:06:48 +0900

    a

--- CCS_Set_NEWNEW/test2.m
+++ CCS_Set_NEWNEW/test2.m
@@ -1,5 +1,13 @@
-A = [1 5 2 4];
-B = [10 50 20 40];
-C = [A' B']
-sortrows(C, 'descend')
-sort(A, 'descend')
+
+sensordata_B0 = fileread('CCS_temporary\@UTST_SenPos.txt');
+sensordata_B =  strsplit(sensordata_B0,{'\n',' ','\r'});
+sensornum_B =   length(sensordata_B)/2 -1 ;
+
+for i=1:sensornum_B
+    R(i) = str2double(sensordata_B{i});
+    Z(i) = str2double(sensordata_B{i+1});
+end
+figure()
+plot(R, Z);
+R = 0;
+Z = 0;
